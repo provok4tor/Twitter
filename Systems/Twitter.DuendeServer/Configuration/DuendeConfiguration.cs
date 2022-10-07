fa11ab759f6638aa37abf8759c3c302aea16ce98
Alexander Varfolomeev <alexander.varfolomei@gmail.com>
2022-10-07 19:56:14 +0300

    Add duende identity server, clientCredentials client

---
+++ Systems/Twitter.DuendeServer/Configuration/DuendeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace Twitter.DuendeServer.Configuration;
+
+public static class DuendeConfiguration
+{
+    public static IServiceCollection AddDuende(this IServiceCollection services)
+    {
+        services.AddIdentityServer(options =>
+        {
+            options.EmitStaticAudienceClaim = true;
+        })
+            .AddInMemoryApiScopes(DuendeConfig.Scopes)
+            .AddInMemoryIdentityResources(DuendeConfig.Resources)
+            .AddInMemoryClients(DuendeConfig.Clients);
+        
+        return services;
+    }
+
+    public static WebApplication UseDuende(this WebApplication app)
+    {
+        app.UseIdentityServer();
+        return app;
+    }
+}
